0e5d40c9f95b045c98547bfcf86a5f561de1577e
Kawasaki <iduchungho@gmail.com>
2023-04-03 15:33:27 +0700

    change host

--- app/app.go
+++ app/app.go
@@ -61,7 +61,7 @@ func (app *App) Run() {
 				panic("Can't run fiber engine")
 			}
 		} else {
-			err := app.r.Listen("localhost:8080")
+			err := app.r.Listen(":8080")
 			if err != nil {
 				panic("Can't run fiber engine")
 			}
